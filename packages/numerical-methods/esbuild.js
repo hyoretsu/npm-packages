c0999071928f7917e00537fdfa1f5b4ebd519318
Aran Leite <hyoretsu@gmail.com>
2023-07-04 17:16:23 -0300

    numerical-methods v0.11.1

--- packages/numerical-methods/esbuild.js
+++ packages/numerical-methods/esbuild.js
@@ -1,21 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-useless-concat */
-const { build } = require('esbuild');
+const { build } = require("esbuild");
+const { dependencies, devDependencies, peerDependencies } = require("./package.json");
 
 const start = Date.now();
 
 try {
-    build({
-        bundle: true,
-        entryPoints: ['./src/'],
-        external: ['./node_modules/*'],
-        format: 'esm',
-        keepNames: true,
-        minify: true,
-        outfile: 'dist/index.mjs',
-        platform: 'node',
-    }).then(() => console.log('⚡ ' + '\x1b[32m' + `Done in ${Date.now() - start}ms`));
+	build({
+		bundle: true,
+		entryPoints: ["./src/"],
+		external: [
+			...(dependencies && Object.keys(dependencies)),
+			...(devDependencies && Object.keys(devDependencies)),
+			...(peerDependencies && Object.keys(peerDependencies)),
+		],
+		keepNames: true,
+		minify: true,
+		outfile: "dist/index.js",
+		platform: "node",
+	}).then(() => console.log("⚡ " + "\x1b[32m" + `Done in ${Date.now() - start}ms`));
 } catch (e) {
-    console.log(e);
-    process.exit(1);
+	console.log(e);
+	process.exit(1);
 }
