ccf1a9bdf13e1019ac6bfe1ad2d82a6f93c1cf09
Aran Leite <hyoretsu@gmail.com>
2023-07-11 17:25:05 -0300

    numerical-methods v0.11.6

--- packages/numerical-methods/src/index.ts
+++ packages/numerical-methods/src/index.ts
@@ -1,3 +1,17 @@
+import * as custom from "./custom";
+import * as functionZeros from "./functionZeros";
+import * as integration from "./integration";
+import * as interpolation from "./interpolation";
+import * as linearSystems from "./linearSystems";
+
+export const organizedMethods = {
+	custom,
+	functionZeros,
+	integration,
+	interpolation,
+	linearSystems,
+};
+
 export * from "./custom";
 export * from "./functionZeros";
 export * from "./integration";
