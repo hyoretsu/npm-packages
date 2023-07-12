de0595cd25e9b43252a7c3d4d159b80f34f73359
Aran Leite <hyoretsu@gmail.com>
2023-07-12 13:21:04 -0300

    @hyoretsu/react-components v1.1.3 and @hyoretsu/rn-components v1.0.0

--- packages/react-components/src/index.ts
+++ packages/react-components/src/index.ts
@@ -2,5 +2,4 @@ export * from "./components/Button";
 export * from "./components/Input";
 export * from "./components/Link";
 export * from "./components/Modal";
-export * from "./components/Pressable";
 export * from "./components/OpacityFilter";
