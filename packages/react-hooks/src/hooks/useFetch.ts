fc8e3670752d5a9a0994e81190248db2eb569fcb
Aran Leite <hyoretsu@gmail.com>
2023-08-11 17:37:15 -0300

    @hyoretsu/react-hooks v1.0.10

--- packages/react-hooks/src/hooks/useFetch.ts
+++ packages/react-hooks/src/hooks/useFetch.ts
@@ -4,16 +4,27 @@ import useSWR from "swr";
 // rome-ignore lint/suspicious/noEmptyInterface: <explanation>
 export interface APIMapping {}
 
-export function useFetch<Route extends keyof APIMapping & string, Error = any>(
+export function useFetch<Route extends keyof APIMapping, Error = any>(
 	urlDeps: Route | [Route, any | any[]],
 	api: AxiosInstance,
-	body?: Record<string, any>,
+	{ body, query }: Record<"body" | "query", Record<string, any> | undefined> = {
+		body: undefined,
+		query: undefined,
+	},
 ) {
 	const reqInfo = useSWR<APIMapping[Route], Error>(urlDeps, async (path: string) => {
 		let res: AxiosResponse<APIMapping[Route]>;
 
 		if (typeof urlDeps !== "string") {
 			[path] = urlDeps;
+		}
+
+		if (query) {
+			path = `${path}?${Object.entries(query).reduce(
+				// rome-ignore lint/style/useTemplate:
+				([key, value], str, index, arr) => str + key + "=" + value + (index !== arr.length - 1 ? "¨&" : ""),
+				"",
+			)}`;
 		}
 
 		if (body) {
