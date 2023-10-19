bdb881f84be7edb6bf96048694a7a1b8e1e9f209
Aran Leite <hyoretsu@gmail.com>
2023-10-18 21:39:32 -0300

    @hyoretsu/rn-components v1.1.3

--- packages/rn-components/src/components/BetterStatusBar/index.tsx
+++ packages/rn-components/src/components/BetterStatusBar/index.tsx
@@ -1,12 +1,12 @@
-import React, { PropsWithChildren } from "react";
+import React from "react";
 import { StatusBar, StatusBarProps, View } from "react-native";
-import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export interface BetterStatusBarProps extends StatusBarProps {
 	backgroundColor?: string;
 }
 
-const BetterStatusBarRaw: React.FC<BetterStatusBarProps> = ({ backgroundColor, ...props }) => {
+export const BetterStatusBar: React.FC<BetterStatusBarProps> = ({ backgroundColor, ...props }) => {
 	const { top: height } = useSafeAreaInsets();
 
 	return (
@@ -15,9 +15,3 @@ const BetterStatusBarRaw: React.FC<BetterStatusBarProps> = ({ backgroundColor, .
 		</View>
 	);
 };
-
-export const BetterStatusBar: React.FC<BetterStatusBarProps> = props => (
-	<SafeAreaProvider>
-		<BetterStatusBarRaw {...props} />
-	</SafeAreaProvider>
-);
