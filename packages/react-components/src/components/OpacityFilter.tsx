5c8866589a0c8a12a706932da4189184c6e9c252
Aran Leite <hyoretsu@gmail.com>
2023-11-15 18:15:07 -0300

    @hyoretsu/react-components v1.6.6

--- packages/react-components/src/components/OpacityFilter.tsx
+++ packages/react-components/src/components/OpacityFilter.tsx
@@ -1,13 +1,23 @@
-import React, { ReactNode } from "react";
+import React, { CSSProperties, ReactNode } from "react";
 
 export interface OpacityFilterProps {
 	children: ReactNode;
 	className?: string;
 	/** Value between 0 and 1. */
 	opacity?: number;
+	style?: CSSProperties;
 }
 
-export const OpacityFilter: React.FC<OpacityFilterProps> = ({ children, className, opacity = 0.5 }) => {
+export const OpacityFilter: React.FC<OpacityFilterProps> = ({
+	children,
+	className,
+	opacity = 0.5,
+	style,
+}) => {
+	if (opacity < 0 || opacity > 1) {
+		throw new Error("The only valid values for opacity are numbers between 0 and 1");
+	}
+
 	return (
 		<div
 			className={className}
@@ -23,6 +33,7 @@ export const OpacityFilter: React.FC<OpacityFilterProps> = ({ children, classNam
 				width: "100%",
 				height: "100%",
 				backgroundColor: `rgba(0, 0, 0, ${opacity})`,
+				...style,
 			}}
 		>
 			{children}
