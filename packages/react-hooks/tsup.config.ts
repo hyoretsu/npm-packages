54fe0c94951a0fdebcf79d1b73daaaf8eb5b8dce
Aran Leite <hyoretsu@gmail.com>
2023-12-08 17:43:43 -0300

    @hyoretsu/react-hooks v1.2.4

--- packages/react-hooks/tsup.config.ts
+++ packages/react-hooks/tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
 	clean: true,
-	entry: ["src/*.ts"],
+	entry: ["src/**/*.ts"],
 	dts: true,
 	format: ["cjs", "esm"],
 	minify: true,
