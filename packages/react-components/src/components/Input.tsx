49060981b9d131263fd952819fa1308432deec40
Aran Leite <hyoretsu@gmail.com>
2024-01-03 10:08:24 -0300

    @hyoretsu/react-components v1.7.6

--- packages/react-components/src/components/Input.tsx
+++ packages/react-components/src/components/Input.tsx
@@ -1,32 +1,70 @@
+"use client";
 import { range } from "@hyoretsu/utils";
-import React, { FormEventHandler, InputHTMLAttributes } from "react";
+import React, { ChangeEvent, ChangeEventHandler, InputHTMLAttributes, useCallback, useState } from "react";
 
-export type CustomInputProps = InputHTMLAttributes<HTMLInputElement>;
+export interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
+	onChange: ChangeEventHandler<HTMLInputElement>;
+}
 
-export const Input: React.FC<CustomInputProps> = ({ type, value, max, min = 0, maxLength, ...rest }) => {
-	const onInput: FormEventHandler<HTMLInputElement> = (e): void => {
-		if (type === "number") {
-			if (maxLength && !max) {
-				max = Number(range(0, maxLength).reduce(str => `${str}9`, ""));
+export const Input: React.FC<CustomInputProps> = ({
+	type,
+	value,
+	max,
+	min = 0,
+	maxLength,
+	onChange,
+	...rest
+}) => {
+	const [internalValue, setInternalValue] = useState(value);
+
+	const handleOnChange = useCallback(
+		(e: ChangeEvent<HTMLInputElement>, send = false): void => {
+			if (!send) {
+				setInternalValue(e.currentTarget.value);
+				return;
 			}
 
-			// @ts-ignore
-			if (Number(e.currentTarget.value) > max) {
-				e.currentTarget.value = String(max);
+			if (type === "number") {
+				if (maxLength && !max) {
+					max = Number(range(0, maxLength).reduce(str => `${str}9`, ""));
+				}
+
+				// @ts-ignore
+				if (Number(e.currentTarget.value) > max) {
+					setInternalValue(String(max));
+				}
+				// @ts-ignore
+				if (Number(e.currentTarget.value) < min) {
+					setInternalValue(String(min));
+				}
+
+				return;
 			}
-			// @ts-ignore
-			if (Number(e.currentTarget.value) < min) {
-				e.currentTarget.value = String(min);
+
+			// Longer than max length
+			if (maxLength && e.currentTarget.value.length >= maxLength) {
+				e.currentTarget.value = e.currentTarget.value.slice(0, maxLength);
 			}
 
-			return;
-		}
+			onChange(e);
+			setInternalValue(value);
+		},
+		[max, maxLength, min, type, onChange, value],
+	);
 
-		// Longer than max length
-		if (maxLength && e.currentTarget.value.length >= maxLength) {
-			e.currentTarget.value = e.currentTarget.value.slice(0, maxLength);
-		}
-	};
-
-	return <input type={type || "text"} value={value} maxLength={maxLength} onInput={onInput} {...rest} />;
+	return (
+		<input
+			type={type || "text"}
+			value={internalValue}
+			maxLength={maxLength}
+			onChange={handleOnChange}
+			onBlur={() => {
+				handleOnChange(
+					{ currentTarget: { value: String(internalValue) } } as ChangeEvent<HTMLInputElement>,
+					true,
+				);
+			}}
+			{...rest}
+		/>
+	);
 };
