61ff49bfc7a9496dcb3bcebb6116222c791098a5
Aran Leite <hyoretsu@gmail.com>
2025-02-04 15:00:22 -0300

    @hyoretsu/providers v1.2.6

--- packages/providers/src/HashProvider/index.ts
+++ packages/providers/src/HashProvider/index.ts
@@ -2,8 +2,9 @@ import { BCryptHashProvider, BCryptJSHashProvider } from "./implementations";
 import type { HashProvider } from "./models";
 
 export type HashProviderKeys = "bcrypt" | "bcryptjs";
+type HashProviders = typeof BCryptHashProvider | typeof BCryptJSHashProvider;
 
-export const hashProviders: Record<HashProviderKeys, typeof HashProvider> = {
+export const hashProviders: Record<HashProviderKeys, HashProviders> = {
 	bcrypt: BCryptHashProvider,
 	bcryptjs: BCryptJSHashProvider,
 };
