d25eea74b5683d3827809ebc579f8fabc1445e1c
Aran Leite <hyoretsu@gmail.com>
2025-12-28 14:01:57 -0300

    @hyoretsu/configs v1.1.9

--- packages/configs/src/lint-staged/index.js
+++ packages/configs/src/lint-staged/index.js
@@ -1,5 +1,7 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-	"*.(css|graphql|js|jsx|json|jsonc|ts|tsx)": ["bun format:biome --no-errors-on-unmatched"],
-	"*.(js|jsx|ts|tsx)": ["bun format:eslint"],
+	"*.(css|graphql|js|jsx|json|jsonc|ts|tsx)": [
+		"npm run biome check --diagnostic-level=error --write --unsafe --no-errors-on-unmatched",
+	],
+	"*.(js|jsx|ts|tsx)": ["npm run eslint --quiet --fix"],
 };
