12991cb8717107b6bb9a420a6c679e65ef2722f8
Aran Leite <hyoretsu@gmail.com>
2026-01-24 01:42:35 -0300

    @hyoretsu/configs v1.1.26

--- packages/configs/src/lint-staged/index.ts
+++ packages/configs/src/lint-staged/index.ts
@@ -2,11 +2,11 @@
 
 type PackageManager = "bun" | "npm" | "pnpm" | "yarn";
 
-const lockCommands: Record<PackageManager, string[]> = {
-	bun: ["bun i --lockfile-only", "git add bun.lock"],
-	npm: ["npm i --package-lock-only", "git add package-lock.json"],
-	pnpm: ["pnpm i --lockfile-only", "git add pnpm-lock.yaml"],
-	yarn: ["yarn install", "git add yarn.lock"], // For Yarn Berry, use: "yarn install --mode update-lockfile"
+const lockCommands: Record<PackageManager, Array<() => string>> = {
+	bun: [() => "bun i --lockfile-only", () => "git add bun.lock"],
+	npm: [() => "npm i --package-lock-only", () => "git add package-lock.json"],
+	pnpm: [() => "pnpm i --lockfile-only", () => "git add pnpm-lock.yaml"],
+	yarn: [() => "yarn install", () => "git add yarn.lock"], // For Yarn Berry, use: "yarn install --mode update-lockfile"
 };
 
 export const lintStagedConfig = (packageManager?: PackageManager) => ({
