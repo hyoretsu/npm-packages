0a3b733c5675253a343126136ccb6f501f611125
Aran Leite <hyoretsu@gmail.com>
2026-03-21 14:54:02 -0300

    @hyoretsu/utils v1.17.1

--- packages/utils/src/funcs/jsonReviver.ts
+++ packages/utils/src/funcs/jsonReviver.ts
@@ -1,9 +1,9 @@
+const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})?$/;
+
 export const jsonReviver = (_key: string, value: any): any => {
 	if (typeof value === "string") {
-		const parsedDate = new Date(value);
-
-		if (!Number.isNaN(parsedDate.getTime())) {
-			return parsedDate;
+		if (isoDateRegex.test(value)) {
+			return new Date(value);
 		}
 	}
 
