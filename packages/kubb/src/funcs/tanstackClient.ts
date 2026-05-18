4f184da7444ab6854f33fd7dd473bf1239acaece
Aran Leite <hyoretsu@gmail.com>
2026-05-18 17:39:12 -0300

    @hyoretsu/kubb v3.0.0

--- packages/kubb/src/funcs/tanstackClient.ts
+++ packages/kubb/src/funcs/tanstackClient.ts
@@ -1,16 +1,21 @@
-import { QueryClient } from "@tanstack/react-query";
 import { timeConversion } from "@hyoretsu/utils";
+import { QueryClient } from "@tanstack/react-query";
 
-export const tanstackClient = new QueryClient({
-	defaultOptions: {
-		mutations: {
-			onSuccess: (data, variables, onMutateResult, context) => {
-				context.client.invalidateQueries({ queryKey: context?.mutationKey });
+export interface TanstackClientData {
+	staleTime?: number;
+}
+
+export const tanstackClient = ({ staleTime }: TanstackClientData = {}) =>
+	new QueryClient({
+		defaultOptions: {
+			mutations: {
+				onSuccess: (data, variables, onMutateResult, context) => {
+					context.client.invalidateQueries({ queryKey: context?.mutationKey });
+				},
+			},
+			queries: {
+				refetchOnWindowFocus: false,
+				staleTime: staleTime ?? timeConversion(1, "minutes", "milliseconds"),
 			},
 		},
-		queries: {
-			refetchOnWindowFocus: false,
-			staleTime: timeConversion(1, "minutes", "milliseconds"),
-		},
-	},
-});
+	});
